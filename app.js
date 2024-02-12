3c11ca029528b45d727892f5928fe5caaa487550
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-12 23:18:25 +0600

    woking properly

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/api/courses', (req, res) => {
+  res.send([1, 2, 3, 4])
+})
+
 app.listen(port, () => {
   console.log(
     colors.rainbow(
