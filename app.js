8b76c29f09acec62cb2f4cdcd98041be8994e32c
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-12 23:17:19 +0600

    setup others

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('hello akaid');
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv')
+const app = express()
+dotenv.config()
+
+const port = process.env.PORT || 8000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(
+    colors.rainbow(
+      `Server is running on port http://localhost:${process.env.PORT}`
+    )
+  )
+})
