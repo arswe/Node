3c439a4928eaba618ce332a69767d2fdf5d93260
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-13 21:08:47 +0600

    mongoose Connection

--- Genres CRUD/backend/database/db.js
+++ Genres CRUD/backend/database/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
 mongoose
-  .connect('mongodb://127.0.0.1:27017')
-  .then(() => console.log('connect mongoDB'))
-  .catch((err) => console.error('error', err))
+  .connect('mongodb://localhost/playground')
+  .then(() => console.log('Connect mongoDB...'))
+  .catch((err) => console.error('Error Connection Fail', err))
