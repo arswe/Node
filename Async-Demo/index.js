4879f97f5348a86df36b54dded3ef4dfbef13be8
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-13 14:52:46 +0600

    setTimeout

--- Async-Demo/index.js
+++ Async-Demo/index.js
@@ -1,7 +1,15 @@
 console.log('Befor')
 
-setTimeout(() => {
-  console.log('Reading a user from database')
-}, 1000)
+const user = gerUser(1)
+
+console.log(user)
 
 console.log('After')
+
+function gerUser(id) {
+  setTimeout(() => {
+    console.log('Reading a user from database')
+    return { id: id, username: 'mosh' }
+  }, 1000)
+  return 1
+}
